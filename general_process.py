621e56288bddb11495725807b607077ec00c223a
dhdzmota <dhdzmota@gmail.com>
2023-01-09 00:56:09 -0600

    Included first commit of project.

---
+++ general_process.py
@@ -0,0 +1,23 @@
+import os
+
+dir = os.path.join(os.path.dirname(__file__))
+
+command_chains = [
+    f'pip install -e {dir}.',
+    f'pip install -r {dir}requirements.txt',
+    f'python {dir}src/data/download_emojis.py',
+    f'python {dir}src/data/process_data.py',
+    f'python {dir}src/features/build_features.py',
+    f'python {dir}src/features/split.py',
+    f'python {dir}src/features/encode_text_information.py',
+    f'python {dir}src/models/train_model.py',
+]
+
+def run_pipeline():
+    for command in command_chains:
+        print(f'Running the following command: "{command}".')
+        os.system(command)
+        print('Done running command.')
+
+if __name__ == '__main__':
+    run_pipeline()
